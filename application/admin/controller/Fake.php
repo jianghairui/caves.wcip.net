0a14c1aee01fa3e8113407ee9ef1d00d19ca8356
Jiang <1873645345@qq.com>
2019-12-17 14:39:10 +0800

    加入了一个临时活动

--- application/admin/controller/Fake.php
+++ application/admin/controller/Fake.php
@@ -149,10 +149,137 @@ class Fake extends Base {
     }
 
     public function roleDetail() {
-
+        $id = input('param.id');
+        $where = [
+            ['u.id','=',$id]
+        ];
+        try {
+            $info = Db::table('mp_user')->alias('u')
+                ->join('mp_user_role r','u.id=r.uid','LEFT')
+                ->field('u.id,u.avatar,u.nickname,u.org,r.role,r.name,r.identity,r.id_front,r.id_back,r.cover,r.tel,r.weixin,r.works,r.license,r.province_code,r.city_code,r.region_code,r.desc')
+                ->where($where)
+                ->find();
+            $whereProvince = [
+                ['pcode','=',0]
+            ];
+            $province_list = Db::table('mp_city')->where($whereProvince)->select();
+            $city_list = [];
+            $region_list = [];
+            if($info['province_code']) {
+                $whereCity = [
+                    ['pcode','=',$info['province_code']]
+                ];
+                $city_list = Db::table('mp_city')->where($whereCity)->select();
+            }
+            if($info['city_code']) {
+                $whereRegion = [
+                    ['pcode','=',$info['city_code']]
+                ];
+                $region_list = Db::table('mp_city')->where($whereRegion)->select();
+            }
+        }catch (\Exception $e) {
+            return ajax($e->getMessage(),-1);
+        }
+        $this->assign('info',$info);
+        $this->assign('qiniu_weburl',config('qiniu_weburl'));
+        $this->assign('province_list',$province_list);
+        $this->assign('city_list',$city_list);
+        $this->assign('region_list',$region_list);
+        return $this->fetch();
     }
 
     public function roleMod() {
+        if(request()->isPost()) {
+            $val['id'] = input('post.id');
+            $val['nickname'] = input('post.nickname');
+            $val['org'] = input('post.org');
+            $val['role'] = input('post.role');
+            $val['role_check'] = 2;
+            $val['create_time'] = time();
+            $val['fake'] = 1;
+
+            $role['org'] = input('post.org');
+            $role['desc'] = input('post.desc');
+            $role['role'] = input('post.role');
+            $role['tel'] = input('post.tel');
+            $role['create_time'] = time();
+            checkInput($val);
+            checkInput($role);
+            $avatar = input('post.avatar');
+            $cover = input('post.cover');
+            if(!$avatar || !$cover) {
+                return ajax('请上传图片',-1);
+            }
+            $role['province_code'] = input('post.provinceCode',0);
+            $role['city_code'] = input('post.cityCode',0);
+            $role['region_code'] = input('post.regionCode',0);
+            if($role['role'] == 3) {
+                if($role['province_code'] == 0 || $role['city_code'] == 0 || $role['region_code'] == 0) {
+                    return ajax('省市区必选',-1);
+                }
+            }
+            try {
+                $whereUser = [
+                    ['id','=',$val['id']],
+                    ['role','=',$val['role']]
+                ];
+                $whereRole = [
+                    ['uid','=',$val['id']],
+                    ['role','=',$val['role']]
+                ];
+                $user_exist = Db::table('mp_user')->where($whereUser)->find();
+                $role_exist = Db::table('mp_user_role')->where($whereRole)->find();
+                if(!$user_exist) { return ajax('非法参数',-1); }
+                if(!$role_exist) { return ajax('非法参数',-1); }
+
+                $qiniu_exist = $this->qiniuFileExist($avatar);
+                if($qiniu_exist !== true) {
+                    return ajax($qiniu_exist['msg'],5);
+                }
+                $qiniu_move = $this->moveFile($avatar,'upload/avatar/');
+
+                if($qiniu_move['code'] == 0) {
+                    $val['avatar'] = $qiniu_move['path'];
+                }else {
+                    return ajax($qiniu_move['msg'],101);
+                }
+
+                $qiniu_exist = $this->qiniuFileExist($cover);
+                if($qiniu_exist !== true) {
+                    return ajax($qiniu_exist['msg'],5);
+                }
+                $qiniu_move = $this->moveFile($cover,'upload/role/');
+
+                if($qiniu_move['code'] == 0) {
+                    $role['cover'] = $qiniu_move['path'];
+                }else {
+                    return ajax($qiniu_move['msg'],101);
+                }
+
+
+                Db::startTrans();
+                Db::table('mp_user')->where($whereUser)->update($val);
+                Db::table('mp_user_role')->where($whereRole)->update($role);
+                Db::commit();
+
+            } catch (\Exception $e) {
+                Db::rollback();
+                if(isset($val['avatar']) && $val['avatar'] != $user_exist['avatar']) {
+                    $this->rs_delete($val['avatar']);
+                }
+                if(isset($role['cover']) && $role['cover'] != $role_exist['cover']) {
+                    $this->rs_delete($role['cover']);
+                }
+                return ajax($e->getMessage(), -1);
+            }
+            if(isset($val['avatar']) && $val['avatar'] != $user_exist['avatar']) {
+                $this->rs_delete($user_exist['avatar']);
+            }
+            if(isset($role['cover']) && $role['cover'] != $role_exist['cover']) {
+                $this->rs_delete($role_exist['cover']);
+            }
+            return ajax();
+        }
 
     }
 
